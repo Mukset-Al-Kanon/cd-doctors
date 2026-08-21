25233b3f5c9d753e0da19ddc6d0b526ca89002ef
kanon <bdllhlkaium@gmail.com>
2026-08-21 09:33:42 +0600

    fix: Clean next.config.mjs to resolve build failure on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**': ['./prisma/dev.db', './dev.db', './prisma/**'],
-    },
-  },
   images: {
     remotePatterns: [
       {
