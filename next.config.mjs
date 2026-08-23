786ac02110133c31b33728f7605d5c99dfabf413
kanon <bdllhlkaium@gmail.com>
2026-08-23 12:08:31 +0600

    fix(config): clean next.config.mjs and tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./prisma/dev.db', './dev.db'],
-    },
-  },
   images: {
     remotePatterns: [
       {
